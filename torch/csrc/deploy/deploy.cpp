43124c93e49b3b8163117a130630c6c63875dc06
Zachary DeVito <zdevito@fb.com>
2021-02-09 12:37:25 -0800

    [deploy] torch::deploy API
    This API allows you to manage multiple python interpreters in a single
    process to deploy PyTorch models packaged with torch.package.
    
    torch/csrc/deploy/deploy.h contains the API definition
    torch/csrc/deploy/test_deploy.cpp has some examples.
    
    Notes:
    * mutex is added to PyTorchStreamReader to make it safe to use from multiple threads at once.
    * USE_DEPLOY is only true for the special libtorch_deployinterpreter.so library, when enabled
      we use a hash table to maintain PyObject <> at::Tensor mappping rather than the internal pointer
      in Tensor since >1 interpreter may have a reference to the tensor.
    * serialization.py has some additional functions for creating pickle objects
      but keeping storages in memory for use transfering tensors between interpreters
    
    ghstack-source-id: 15cfbfb7fef88fcd621775d4796e9b021a42c033
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51754
    
    mypy fixes, windows fixes
    
    ghstack-source-id: 15cfbfb7fef88fcd621775d4796e9b021a42c033
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51798

---
+++ torch/csrc/deploy/deploy.cpp
@@ -0,0 +1,95 @@
+#include <torch/csrc/deploy/deploy.h>
+
+#include <dlfcn.h>
+#include <libgen.h>
+#include <unistd.h>
+
+extern "C" char _binary_libtorch_deployinterpreter_so_start[];
+extern "C" char _binary_libtorch_deployinterpreter_so_end[];
+
+namespace torch {
+
+Package InterpreterManager::load_package(const std::string& uri) {
+  return Package(uri, this);
+}
+
+InterpreterSession MovableObject::acquire_session(
+    const Interpreter* on_this_interpreter) {
+  InterpreterSession I = on_this_interpreter
+      ? on_this_interpreter->acquire_session()
+      : pImpl_->manager_->acquire_one();
+  I.self = I.impl_->unpickle_or_get(pImpl_->object_id_, pImpl_->data_);
+  return I;
+}
+
+void MovableObjectImpl::unload(const Interpreter* on_this_interpreter) {
+  if (!on_this_interpreter) {
+    for (auto& interp : manager_->all_instances()) {
+      unload(&interp);
+    }
+    return;
+  }
+
+  InterpreterSession I = on_this_interpreter->acquire_session();
+  I.impl_->unload(object_id_);
+}
+
+MovableObjectImpl::~MovableObjectImpl() {
+  unload(nullptr);
+}
+
+void MovableObject::unload(const Interpreter* on_this_interpreter) {
+  pImpl_->unload(on_this_interpreter);
+}
+
+MovableObject InterpreterSession::create_movable(PythonObject obj) {
+  TORCH_CHECK(
+      manager_,
+      "Can only create a movable object when the session was created from an interpreter that is part of a InterpreterManager");
+  auto pickled = impl_->pickle(self, obj);
+  return MovableObject(std::make_shared<MovableObjectImpl>(
+      manager_->next_object_id_++, std::move(pickled), manager_));
+}
+
+Interpreter::Interpreter(InterpreterManager* manager)
+    : handle_(nullptr), manager_(manager) {
+  char library_name[L_tmpnam];
+  std::tmpnam(library_name);
+  library_name_ = library_name;
+  {
+    std::ofstream dst(library_name, std::ios::binary);
+    dst.write(
+        _binary_libtorch_deployinterpreter_so_start,
+        _binary_libtorch_deployinterpreter_so_end -
+            _binary_libtorch_deployinterpreter_so_start);
+  }
+  handle_ = dlopen(library_name, RTLD_LOCAL | RTLD_LAZY);
+  if (!handle_) {
+    throw std::runtime_error(dlerror());
+  }
+
+  // technically, we can unlink the library right after dlopen, and this is
+  // better for cleanup because even if we crash the library doesn't stick
+  // around. However, its crap for debugging because gdb can't find the
+  // symbols if the library is no longer present.
+
+  unlink(library_name_.c_str());
+
+  void* new_interpreter_impl = dlsym(handle_, "new_interpreter_impl");
+  assert(new_interpreter_impl);
+  pImpl_ = std::unique_ptr<InterpreterImpl>(
+      ((InterpreterImpl * (*)(void)) new_interpreter_impl)());
+}
+
+Interpreter::~Interpreter() {
+  if (handle_) {
+    // ensure python uninitialization runs before we dlclose the library
+    pImpl_.reset();
+    // it segfaults its face off trying to unload, but it's not clear
+    // if this is something we caused of if libtorch_python would also do the
+    // same if it were opened/closed a lot...
+    dlclose(handle_);
+  }
+}
+
+} // namespace torch
